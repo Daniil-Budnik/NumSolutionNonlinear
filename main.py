bf7ecda4802137f29feb2966d15dd0496b025426
Saigen <pc-set@inbox.ru>
2020-11-02 16:41:03 +0300

    Example Gausse

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from Numerical import *
-#  Графики функции http://yotx.ru/
-
-
-def _F(X): return (5 * (X**3) - (X ** 2) - 20 * X + 4)
-def _dF(X): return (15 * (X ** 2) - 2 * X - 20)
 
 # Главный метод
 if __name__ == '__main__':
-    print("Метод половинного деления: ", Half_Division( -10, 0, _F, E=0.00001 ) )
-    print("Метод Ньютона: ", Tangent_Method( -10, 0, _F, _dF , E=0.00001 ) )
+    X = [[2,-1,0],
+         [-1,1,4],
+         [1,2,3]]
+
+    Y = [0,13,14] 
+
+    print(Gauss(X,Y)) # Решение 3 2 1
 
 
 
