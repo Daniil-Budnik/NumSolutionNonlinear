65b4d2bc248cc87a9d4dfb2bb6dff9a5d5ea36b2
Saigen <pc-set@inbox.ru>
2020-12-16 05:17:12 +0400

    Update Spline.py

--- Spline.py
+++ Spline.py
@@ -1,15 +1,22 @@
 from NumSolutionNonlinear import *
 from matplotlib.pyplot import*
 
+
 #A = np.loadtxt("_A.txt")
 #B = np.loadtxt("_B.txt")
 #print(Sweep(A,B))
 
-T = np.arange(0,1.1,0.01)
-Tk = np.arange(0,1,0.003)
-X = [ np.sin(x*10)*5 for x in T]
+#T = np.arange(0,1.01,0.01)
+#Tk = np.arange(0.1,1,0.001)
+#X = [ np.sin(x*10)*5 for x in T]
 
-plot(T,X,'blue')
+T = np.loadtxt("T.txt")
+X = np.loadtxt("X.txt")
+Tk = np.arange(-2.9,2.9,0.001)
+
+
+scatter(T,X)
 plot(Tk,Interpol(T,X, Tk),'green')
+
 show()
 
